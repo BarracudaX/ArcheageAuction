1ac8f1b020c051fd0404131452a4c7eb31c70f59
BarracudaX <dimitrijchannel@gmail.com>
2023-10-17 21:14:38 +0300

    Remove spring boot plugin from root project that cause build failures

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,4 @@
 plugins {
-    id("org.springframework.boot") version "3.1.4"
-    id("io.spring.dependency-management") version "1.1.3"
     kotlin("jvm") version "1.8.21"
     kotlin("plugin.spring") version "1.8.21"
     kotlin("plugin.jpa") version "1.8.21"
