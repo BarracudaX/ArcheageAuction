f1432c3233859bad2373fc724e7acda0d258d6c8
BarracudaX <dimitrijchannel@gmail.com>
2023-10-28 16:36:51 +0300

    Improve packs view
    1)Added more messages to messages bundle.
    2)Improved packs view - added collapsible that shows recipe information for every row.

--- src/main/kotlin/com/arslan/archeage/controllers/ViewController.kt
+++ src/main/kotlin/com/arslan/archeage/controllers/ViewController.kt
@@ -2,6 +2,7 @@ package com.arslan.archeage.controllers
 
 import com.arslan.archeage.Continent
 import com.arslan.archeage.PackDTO
+import com.arslan.archeage.RecipeDTO
 import com.arslan.archeage.entity.CraftingMaterial
 import com.arslan.archeage.entity.ItemPrice
 import com.arslan.archeage.entity.PackPrice
@@ -16,30 +17,36 @@ import org.springframework.web.bind.annotation.RequestMapping
 
 @RequestMapping("/")
 @Controller
-class ViewController(private val locationService: LocationService, private val packService: PackService,private val itemPriceService: ItemPriceService) {
+class ViewController(
+    private val locationService: LocationService,
+    private val packService: PackService,
+    private val itemPriceService: ItemPriceService
+) {
 
     @GetMapping
-    fun homePage() : String = "index"
+    fun homePage(): String = "index"
 
     @GetMapping("/packs")
-    fun packsPage(model: Model) : String {
+    fun packsPage(model: Model): String {
         val continent = Continent.values()[0]
         val packs = packService.packs(continent)
         val prices = itemPriceService
             .latestPrices(packs.flatMap { pack -> pack.recipes.flatMap { recipe -> recipe.materials.map(CraftingMaterial::item) } })
-            .associateBy{ itemPrice -> itemPrice.item.name }
+            .associateBy { itemPrice -> itemPrice.item.name }
 
         val packDTOs = packs.flatMap { pack ->
-            pack.recipes.map { recipe -> recipe to pack }
-        }.flatMap { (recipe,pack) ->
-            pack.prices.filterIsInstance<PackPrice>().map { price -> Triple(pack,recipe,price) }
-        }.map { (pack,recipe,price) ->
-            PackDTO(pack.name,pack.creationLocation.name,price.sellLocation.name,price.price,recipe.toDTO(prices))
+            pack.prices
+                .filterIsInstance<PackPrice>()
+                .flatMap { packPrice ->
+                    pack.recipes.map { recipe ->
+                        PackDTO(pack.name, pack.creationLocation.name, packPrice.sellLocation.name, packPrice.price, RecipeDTO(recipe.producedQuantity,recipe.materials.map { material -> material.toDTO(prices) },recipe.id!!))
+                    }
+                }
         }.sortedByDescending(PackDTO::profit)
 
-        model.addAttribute("locations",locationService.continentLocations(continent))
-        model.addAttribute("factories",locationService.continentFactories(continent))
-        model.addAttribute("packs",packDTOs)
+        model.addAttribute("locations", locationService.continentLocations(continent))
+        model.addAttribute("factories", locationService.continentFactories(continent))
+        model.addAttribute("packs", packDTOs)
         return "packs"
     }
 }
