1dbf5bcec878cd1daa90d6f6cfc75d5092b0b6fc
BarracudaX <dimitrijchannel@gmail.com>
2023-12-23 18:11:44 +0200

    Fix index template not rendering
    1)Fixed index page not rendering because additional parameter wasn't defined for it by
     a)Renamed index.html into common.html
     b)Created new index.html that uses common.html to include fragments like other templates.

--- src/main/kotlin/com/arslan/archeage/controllers/ViewController.kt
+++ src/main/kotlin/com/arslan/archeage/controllers/ViewController.kt
@@ -1,21 +1,13 @@
 package com.arslan.archeage.controllers
 
 import com.arslan.archeage.Continent
-import com.arslan.archeage.CraftingMaterialDTO
-import com.arslan.archeage.PackDTO
 import com.arslan.archeage.entity.*
-import com.arslan.archeage.materialsWithPrice
-import com.arslan.archeage.service.ItemPriceService
-import com.arslan.archeage.service.LocationService
-import com.arslan.archeage.service.PackService
-import org.apache.commons.lang3.arch.Processor.Arch
 import org.springframework.data.domain.Pageable
 import org.springframework.http.MediaType
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
 import java.util.Optional
 import kotlin.jvm.optionals.getOrElse
 
