93a28b0d9dddfce82eb889fb6da75c3dcd5d3f8c
BarracudaX <dimitrijchannel@gmail.com>
2023-12-27 14:44:53 +0200

    Add detekt static analysis tool

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,10 +3,12 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "3.2.0"
     id("io.spring.dependency-management") version "1.1.4"
-    kotlin("jvm") version "1.8.21"
-    kotlin("plugin.spring") version "1.8.21"
-    kotlin("plugin.jpa") version "1.8.21"
+    id("io.gitlab.arturbosch.detekt") version("1.23.3")
+    kotlin("jvm") version "1.9.10"
+    kotlin("plugin.spring") version "1.9.10"
+    kotlin("plugin.jpa") version "1.9.10"
     kotlin("plugin.serialization") version "1.9.0"
+
 }
 
 group = "com.arslan"
