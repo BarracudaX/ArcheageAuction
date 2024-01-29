18e91accc78fbbba4ce7173957739fdbf1fc2835
BarracudaX <dimitrijchannel@gmail.com>
2024-01-30 01:16:13 +0200

    Return back the previous configuration of docker compose and add test conatiners log consumer that outputs the result to logger

--- src/test/kotlin/com/arslan/archeage/service/AbstractITest.kt
+++ src/test/kotlin/com/arslan/archeage/service/AbstractITest.kt
@@ -2,6 +2,7 @@ package com.arslan.archeage.service
 
 import com.arslan.archeage.AbstractTest
 import com.arslan.archeage.repository.*
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestEntityManager
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager
@@ -14,6 +15,7 @@ import org.springframework.test.context.TestConstructor
 import org.springframework.test.jdbc.JdbcTestUtils
 import org.springframework.transaction.annotation.Transactional
 import org.testcontainers.containers.MySQLContainer
+import org.testcontainers.containers.output.Slf4jLogConsumer
 import org.testcontainers.elasticsearch.ElasticsearchContainer
 
 @Transactional
@@ -22,6 +24,7 @@ import org.testcontainers.elasticsearch.ElasticsearchContainer
 @AutoConfigureTestEntityManager
 @TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
 abstract class AbstractITest : AbstractTest(){
+
 
     @Autowired
     protected lateinit var packRepository: PackRepository
@@ -59,8 +62,11 @@ abstract class AbstractITest : AbstractTest(){
 
     companion object{
 
+        private val containerLoggerConsumer = Slf4jLogConsumer(LoggerFactory.getLogger("Integration Test Containers"))
+
         @JvmStatic
-        val mysql = MySQLContainer("mysql:latest").withReuse(true)
+        val mysql = MySQLContainer("mysql:latest")
+            .withReuse(true)
 
         @JvmStatic
         val elasticSearch = ElasticsearchContainer("elasticsearch:8.11.1")
@@ -73,6 +79,7 @@ abstract class AbstractITest : AbstractTest(){
         fun dynamicProperties(registry: DynamicPropertyRegistry){
             mysql.start()
             elasticSearch.start()
+            mysql.followOutput(containerLoggerConsumer)
             registry.add("spring.datasource.url"){ mysql.jdbcUrl }
             registry.add("spring.datasource.username"){ mysql.username }
             registry.add("spring.datasource.password"){ mysql.password }
